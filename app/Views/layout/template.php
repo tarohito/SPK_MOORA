2f485ce89728d4e8696c30fac0ccb7d825a815e6
Taro <filro39@gmail.com>
2024-07-20 19:10:54 +0700

    Final

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -135,7 +135,6 @@ $uri = service('uri');
    <script src="assets/js/jquery.min.js"></script>
 
    <script src="assets/DataTables/datatables.min.js"></script>
-   <!-- <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script> -->
 
    <script src="assets/js/popper.min.js"></script>
    <script src="assets/js/bootstrap.min.js"></script>
